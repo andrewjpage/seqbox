48cb18280c0a80ff906a09e37fd87c086ff17ff9
flashton2003 <flashton@gmail.com>
2021-06-22 14:25:03 +0200

    modified checking query

--- test/03.test/checking_outputs.sql
+++ test/03.test/checking_outputs.sql
@@ -17,3 +17,15 @@ join sample_source ss on sample.sample_source_id = ss.id
 join sample_source_project ssp on ss.id = ssp.sample_source_id
 join project p on ssp.project_id = p.id
 where project_name = any(array['PseudomonasSeq', 'AcinetobacterSeq']);
+
+
+# PTS89D_sample2 should have two extractions, with different extraction identifiers
+select readset_identifier, sample_identifier, extraction_identifier, date_extracted, e.id from read_set_nanopore
+join read_set rs on rs.id = read_set_nanopore.readset_id
+join raw_sequencing r on rs.raw_sequencing_id = r.id
+join extraction e on r.extraction_id = e.id
+join sample s on e.sample_id = s.id
+join sample_source ss on s.sample_source_id = ss.id
+join sample_source_project ssp on ss.id = ssp.sample_source_id
+join project p on ssp.project_id = p.id
+where project_name = any(array['PseudomonasSeq', 'AcinetobacterSeq']);
