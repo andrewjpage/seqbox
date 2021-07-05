8a1dc5270f845a8e4c4c3c80f7e1c50b63a2c9ce
flashton2003 <flashton@gmail.com>
2021-07-05 15:53:25 +0200

    added pangolin results to test 01

--- test/run_test_01.sh
+++ test/run_test_01.sh
@@ -16,3 +16,4 @@ python src/scripts/seqbox_cmd.py add_raw_sequencing_batches -i test/01.test_todo
 python src/scripts/seqbox_cmd.py add_readset_batches -i test/01.test_todo_list_query/readset_batches.csv
 python src/scripts/seqbox_cmd.py add_readsets -i test/01.test_todo_list_query/nanopore_default_readsets.csv -c test/test_seqbox_config.yaml -s -n 
 python src/scripts/seqbox_cmd.py add_artic_covid_results -i test/01.test_todo_list_query/artic_nf_covid_results.csv -b 20201201_1355_MN33881_FAO20804_109641e0 -w medaka -p docker
+python src/scripts/seqbox_cmd.py add_pangolin_results -i test/01.test_todo_list_query/pangolin_results.csv -b 20201201_1355_MN33881_FAO20804_109641e0 -w medaka -p docker
