1074ccee6a4d61ca1046ff95709f4abfff98fef5
flashton2003 <flashton@gmail.com>
2021-07-06 15:42:00 +0200

    split out the filehandling into different script

--- test/run_test_01.sh
+++ test/run_test_01.sh
@@ -14,6 +14,6 @@ python src/scripts/seqbox_cmd.py add_tiling_pcrs -i test/01.test_todo_list_query
 python src/scripts/seqbox_cmd.py add_covid_confirmatory_pcrs -i test/01.test_todo_list_query/confirmatory_pcr.csv
 python src/scripts/seqbox_cmd.py add_raw_sequencing_batches -i test/01.test_todo_list_query/raw_sequencing_batch.csv
 python src/scripts/seqbox_cmd.py add_readset_batches -i test/01.test_todo_list_query/readset_batches.csv
-python src/scripts/seqbox_cmd.py add_readsets -i test/01.test_todo_list_query/nanopore_default_readsets.csv -c test/test_seqbox_config.yaml -s -n 
+python src/scripts/seqbox_cmd.py add_readsets -i test/01.test_todo_list_query/nanopore_default_readsets.csv -s -n 
 python src/scripts/seqbox_cmd.py add_artic_covid_results -i test/01.test_todo_list_query/artic_nf_covid_results.csv -b 20201201_1355_MN33881_FAO20804_109641e0 -w medaka -p docker
 python src/scripts/seqbox_cmd.py add_pangolin_results -i test/01.test_todo_list_query/pangolin_results.csv -b 20201201_1355_MN33881_FAO20804_109641e0 -w medaka -p docker
