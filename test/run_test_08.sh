4e7e8c4a7de0ecf8cd4475f41e83b7184758784b
flashton2003 <flashton@gmail.com>
2022-12-23 09:20:22 +0200

    test08 working through to add_readsets

--- test/run_test_08.sh
+++ test/run_test_08.sh
@@ -9,8 +9,8 @@ python src/scripts/seqbox_cmd.py add_sample_sources -i test/08.test/submission_t
 python src/scripts/seqbox_cmd.py add_samples -i test/08.test/submission_tracker.csv
 python src/scripts/seqbox_cmd.py add_extractions -i test/08.test/submission_tracker.csv
 
-#python src/scripts/seqbox_cmd.py add_raw_sequencing_batches -i test/06.test/raw_sequencing_batch.csv
-#python src/scripts/seqbox_cmd.py add_readset_batches -i test/06.test/readset_batches.csv
-#python src/scripts/seqbox_cmd.py add_readsets -i test/06.test/nanopore_readsets.csv
+python src/scripts/seqbox_cmd.py add_raw_sequencing_batches -i test/08.test/raw_sequencing_batch.csv
+python src/scripts/seqbox_cmd.py add_readset_batches -i test/08.test/readset_batches.csv
+python src/scripts/seqbox_cmd.py add_readsets -i test/08.test/illumina_readsets.csv
 #python src/scripts/seqbox_filehandling.py add_readset_to_filestructure -i test/06.test/nanopore_readsets.csv -c test/test_seqbox_config.yaml
 
