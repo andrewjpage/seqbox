f1359e250e958b182a82d2500ce5f564bf247624
Andrew Page <andrewjpage@gmail.com>
2023-08-30 10:32:08 +0000

    Test readset batches

--- src/tests/unit/test_seqbox_utils_readset_batches.py
+++ src/tests/unit/test_seqbox_utils_readset_batches.py
@@ -6,9 +6,9 @@ sys.path.append('../')
 sys.path.append('./')
 sys.path.append('../scripts')
 from app import app, db
-from seqbox_utils import  get_readset_batch, add_readset_batch, read_in_readset_batch, check_readset_batches
+from seqbox_utils import  get_readset_batch, add_readset_batch, check_readset_batches, add_raw_sequencing_batch
 
-class TestSeqboxUtilsRawSequencingBatches(TestCase):
+class TestSeqboxUtilsReadSetBatches(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
@@ -18,6 +18,9 @@ class TestSeqboxUtilsRawSequencingBatches(TestCase):
         assert os.environ['DATABASE_URL'].split('/')[3].startswith('test') # you really dont want to delete a production DB
         db.drop_all()
         db.create_all()
+
+    def populate_db(self):
+        add_raw_sequencing_batch({'date_run': '01/01/2023', 'sequencing_type': 'nanopore', 'batch_name': 'ABC', 'instrument_model': 'MinION', 'instrument_name': 'ABC123', 'sequencing_centre': 'MLW', 'flowcell_type': 'R12.3', 'sequencing_type': 'WGS', 'batch_directory': '/path/to/dir' })
 
     def test_check_readset_batches(self):
         self.assertTrue(check_readset_batches({'raw_sequencing_batch_name': 'ABC', 'readset_batch_name': 'EFG', 'readset_batch_dir': '/path/to/dir', 'basecaller': 'guppy' }))
@@ -39,3 +42,48 @@ class TestSeqboxUtilsRawSequencingBatches(TestCase):
             check_readset_batches({'raw_sequencing_batch_name': 'ABC', 'readset_batch_name': 'EFG',  'basecaller': 'guppy' })
         with self.assertRaises(SystemExit) as cm:
             check_readset_batches({'raw_sequencing_batch_name': 'ABC', 'readset_batch_name': 'EFG', 'readset_batch_dir': '/path/to/dir' })
+
+    def test_get_readset_batch_empty_db(self):
+        self.setUp()
+        readset_batch_info = {'raw_sequencing_batch_name': 'ABC', 
+                              'readset_batch_name': 'EFG', 
+                              'readset_batch_dir': '/path/to/dir', 
+                              'basecaller': 'guppy' 
+                            }
+        self.assertEqual(get_readset_batch(readset_batch_info), False)
+
+    def test_add_readset_batch(self):
+        self.setUp()
+        self.populate_db()
+        readset_batch_info = {'raw_sequencing_batch_name': 'ABC', 
+                              'readset_batch_name': 'EFG', 
+                              'readset_batch_dir': '/path/to/dir', 
+                              'basecaller': 'guppy' 
+                            }
+        add_readset_batch(readset_batch_info)
+        batch = get_readset_batch(readset_batch_info)
+        self.assertTrue(batch.name, 'EFG')
+
+    def test_add_readset_batch_twice(self):
+        self.setUp()
+        self.populate_db()
+        readset_batch_info = {'raw_sequencing_batch_name': 'ABC', 
+                              'readset_batch_name': 'EFG', 
+                              'readset_batch_dir': '/path/to/dir', 
+                              'basecaller': 'guppy' 
+                            }
+        add_readset_batch(readset_batch_info)
+        add_readset_batch(readset_batch_info)
+        with self.assertRaises(SystemExit) as cm:
+            get_readset_batch(readset_batch_info)
+
+    def test_add_readset_batch_raw_sequencing_batch_doesnt_exist(self):
+        self.setUp()
+        self.populate_db()
+        readset_batch_info = {'raw_sequencing_batch_name': 'doesnt_exist', 
+                              'readset_batch_name': 'EFG', 
+                              'readset_batch_dir': '/path/to/dir', 
+                              'basecaller': 'guppy' 
+                            }
+        with self.assertRaises(SystemExit) as cm:
+            add_readset_batch(readset_batch_info)
